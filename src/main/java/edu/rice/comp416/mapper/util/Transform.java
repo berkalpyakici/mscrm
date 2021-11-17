9b5ce11311527d723e6617bf4d77aa226a8ea93f
Berk Alp Yakici <contact@berkalpyakici.com>
2021-11-16 18:17:26 -0600

    Add method for generating kmers.

--- src/main/java/edu/rice/comp416/mapper/util/Transform.java
+++ src/main/java/edu/rice/comp416/mapper/util/Transform.java
@@ -1,5 +1,9 @@
 package edu.rice.comp416.mapper.util;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 /** Utilities for string transformations. */
 public class Transform {
 
@@ -8,8 +12,10 @@ public class Transform {
      *
      * @param s Input sequence.
      * @return Reverse complement of the input sequence.
+     * @throws UnsupportedEncodingException If the input sequence contains characters other than
+     *     A,T,C,G.
      */
-    public static String reverseComplement(String s) {
+    public static String getReverseComplement(String s) throws UnsupportedEncodingException {
         StringBuilder r = new StringBuilder();
 
         for (int i = s.length() - 1; i >= 0; i--) {
@@ -27,11 +33,32 @@ public class Transform {
                     r.append('C');
                     break;
                 default:
-                    r.append('-');
-                    break;
+                    throw new UnsupportedEncodingException(
+                            "The input sequence can only contain A,T,C,G.");
             }
         }
 
         return r.toString();
+    }
+
+    /**
+     * Get an ordered list of k-mers of given size from the input sequence.
+     *
+     * @param s Input sequence.
+     * @param k K-mer size.
+     * @return Ordered list of k-mers.
+     */
+    public static List<String> getKmers(String s, int k) {
+        List<String> kmers = new ArrayList<>();
+
+        if (k > s.length() || k <= 0) {
+            return kmers;
+        }
+
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            kmers.add(s.substring(i, i + k));
+        }
+
+        return kmers;
     }
 }
--- src/test/java/edu/rice/comp416/mapper/util/TransformTest.java
+++ src/test/java/edu/rice/comp416/mapper/util/TransformTest.java
@@ -1,12 +1,37 @@
 package edu.rice.comp416.mapper.util;
 
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TransformTest {
     @Test
-    public void testReverseComplement() {
-        Assert.assertEquals("GTACCT", Transform.reverseComplement("AGGTAC"));
-        Assert.assertEquals("AATGTACTA", Transform.reverseComplement("TAGTACATT"));
+    public void testGetReverseComplement() throws Exception {
+        Assert.assertEquals("GTACCT", Transform.getReverseComplement("AGGTAC"));
+        Assert.assertEquals("AATGTACTA", Transform.getReverseComplement("TAGTACATT"));
+    }
+
+    @Test
+    public void testGetKmersABCDEFG() {
+        List<String> test1 = Transform.getKmers("abcdefg", 3);
+
+        Assert.assertEquals(5, test1.size());
+        Assert.assertTrue(test1.contains("abc"));
+        Assert.assertTrue(test1.contains("bcd"));
+        Assert.assertTrue(test1.contains("cde"));
+        Assert.assertTrue(test1.contains("def"));
+        Assert.assertTrue(test1.contains("efg"));
+    }
+
+    @Test
+    public void testGetKmersEdgeCases() {
+        List<String> test1 = Transform.getKmers("abcdefg", 7);
+
+        Assert.assertEquals(1, test1.size());
+        Assert.assertTrue(test1.contains("abcdefg"));
+
+        List<String> test2 = Transform.getKmers("abcdefg", 8);
+
+        Assert.assertEquals(0, test2.size());
     }
 }
